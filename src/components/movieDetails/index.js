5c659da98476bad363cd82d7db3f1d0068982729
Sophia_McGee <20040472@mail.wit.ie>
2022-06-30 15:03:45 +0100

    Show movie review excerpts; Refactor Details page.

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
-import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import MonetizationIcon from "@material-ui/icons/MonetizationOn";
 import StarRate from "@material-ui/icons/StarRate";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+// New
+import NavigationIcon from "@material-ui/icons/Navigation";
+import Fab from "@material-ui/core/Fab";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from '../movieReviews'
 
 const useStyles = makeStyles((theme) => ({
   chipRoot: {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "screenLeft",
+    alignItems: "center",
     flexWrap: "wrap",
     listStyle: "none",
     padding: theme.spacing(1.5),
@@ -31,78 +35,64 @@ const useStyles = makeStyles((theme) => ({
   chipLabel: {
     margin: theme.spacing(0.5),
   },
-  chipReview: {
-    position: "absolute",
-    right: 20,
-    bottom: 20,
+  fab: {  //New
+    position: "fixed",
+    top: theme.spacing(15),
+    right: theme.spacing(2),
   },
 }));
 
-const MovieDetails = (props) => {
+const MovieDetails = ( {movie}) => {
   const classes = useStyles();
-  const movie = props.movie;
+  const [drawerOpen, setDrawerOpen] = useState(false); // New
 
   return (
     <>
       <Typography variant="h5" component="h3">
         Overview
       </Typography>
+
       <Typography variant="h6" component="p">
         {movie.overview}
       </Typography>
       <div className={classes.chipRoot}>
-        <Paper component="ul" className={classes.chipSet}>
-          <li>
-            <Chip
-              label="Genres"
-              className={classes.chipLabel}
-              color="primary"
-            />
+      <Paper component="ul" className={classes.chipSet}>
+        <li>
+          <Chip label="Genres" className={classes.chipLabel} color="primary" />
+        </li>
+        {movie.genres.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} className={classes.chip} />
           </li>
-          <li>
-            <Chip label="Science Fiction" className={classes.chipLabel} />
-          </li>
-          <li>
-            <Chip label="Action" className={classes.chipLabel} />
-          </li>
-        </Paper>
-        <Paper component="ul" className={classes.chipSet}>
-          <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-          <Chip
-            icon={<MonetizationIcon />}
-            label={`${movie.revenue.toLocaleString()}`}
-          />
-          <Chip
-            icon={<StarRate />}
-            label={`${movie.vote_average} (${movie.vote_count}`}
-          />
-          <Chip label={`Released: ${movie.release_date}`} />
-        </Paper>
-        <Paper component="ul" className={classes.chipSet}>
-          <li>
-            <Chip
-              label="Production Countries"
-              className={classes.chipLabel}
-              color="primary"
-            />
-          </li>
-          <li>
-            <Chip label="France" className={classes.chipLabel} />
-          </li>
-          <li>
-            <Chip label="Lithuania" className={classes.chipLabel} />
-          </li>
-        </Paper>
-        <Paper>
-          <Chip
-            icon={<ArrowDropUpIcon />}
-            label="REVIEWS"
-            color="secondary"
-            className={classes.chipReview}
-          />
-        </Paper>
+        ))}
+      </Paper>
+      <Paper component="ul" className={classes.chipSet}>
+        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+        <Chip
+          icon={<MonetizationIcon />}
+          label={`${movie.revenue.toLocaleString()}`}
+        />
+        <Chip
+          icon={<StarRate />}
+          label={`${movie.vote_average} (${movie.vote_count}`}
+        />
+        <Chip label={`Released: ${movie.release_date}`} />
+      </Paper>
       </div>
+      {/* New */}
+      <Fab    
+        color="secondary"
+        variant="extended"
+        onClick={() =>setDrawerOpen(true)}
+        className={classes.fab}
+      >
+        <NavigationIcon />
+        Reviews
+      </Fab>
+      <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+        <MovieReviews movie={movie} />
+      </Drawer>
     </>
   );
 };
-export default MovieDetails;
+export default  MovieDetails;
